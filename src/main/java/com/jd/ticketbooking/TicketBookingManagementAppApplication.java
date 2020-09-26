eae5a01899b6f91d646175661baf1c724e71052d
jd-gupta27 <jd.gupta27@yahoo.in>
2020-09-26 14:08:27 +0530

    second commit

--- src/main/java/com/jd/ticketbooking/TicketBookingManagementAppApplication.java
+++ src/main/java/com/jd/ticketbooking/TicketBookingManagementAppApplication.java
@@ -10,9 +10,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.jd.ticketbooking.entity.Ticket;
 import com.jd.ticketbooking.service.TicketBookingService;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TicketBookingManagementAppApplication implements CommandLineRunner {
+public class TicketBookingManagementAppApplication extends SpringBootServletInitializer implements CommandLineRunner {
 	@Autowired
 	private TicketBookingService ticketBookingService;
 	
@@ -20,6 +22,10 @@ public class TicketBookingManagementAppApplication implements CommandLineRunner 
 		SpringApplication.run(TicketBookingManagementAppApplication.class, args);
 	}
 
+	    @Override
+	    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+            }
 	@Override
 	public void run(String... args) throws Exception {
 		Ticket ticket = new Ticket();
